d6cae4a95fb224d89fda0ca2efdff0d2ba536c70
mengxin.wu <mengxin.wu@cootek.cn>
2022-08-17 13:58:57 +0800

    add and fix

--- cmd/executor/main.go
+++ cmd/executor/main.go
@@ -2,37 +2,41 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/Shopify/sarama"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+	//创建新的消费者
 	consumer, err := sarama.NewConsumer([]string{"kafka0:9092", "kafka1:9093"}, nil)
 	if err != nil {
-		fmt.Printf("fail to start consumer, err:%v\n", err)
-		return
+		fmt.Println("fail to start consumer", err)
 	}
-	partitionList, err := consumer.Partitions("web_log") // 根据topic取到所有的分区
+	//根据topic获取所有的分区列表
+	partitionList, err := consumer.Partitions("weatherStation")
 	if err != nil {
-		fmt.Printf("fail to get list of partition:err%v\n", err)
-		return
+		fmt.Println("fail to get list of partition,err:", err)
 	}
 	fmt.Println(partitionList)
-	for partition := range partitionList { // 遍历所有的分区
-		// 针对每个分区创建一个对应的分区消费者
-		pc, err := consumer.ConsumePartition("web_log", int32(partition), sarama.OffsetNewest)
+	//遍历所有的分区
+	for p := range partitionList {
+		//针对每一个分区创建一个对应分区的消费者
+		pc, err := consumer.ConsumePartition("weatherStation", int32(p), sarama.OffsetNewest)
 		if err != nil {
-			fmt.Printf("failed to start consumer for partition %d,err:%v\n", partition, err)
-			return
+			fmt.Printf("failed to start consumer for partition %d,err:%v\n", p, err)
 		}
 		defer pc.AsyncClose()
-		// 异步从每个分区消费信息
+		wg.Add(1)
+		//异步从每个分区消费信息
 		go func(sarama.PartitionConsumer) {
 			for msg := range pc.Messages() {
-				fmt.Printf("Partition:%d Offset:%d Key:%v Value:%v", msg.Partition, msg.Offset, msg.Key, msg.Value)
+				fmt.Printf("partition:%d Offse:%d Key:%v Value:%s \n",
+					msg.Partition, msg.Offset, msg.Key, msg.Value)
 			}
 		}(pc)
 	}
-
-	select {}
+	wg.Wait()
 }
--- cmd/monitor/main.go
+++ cmd/monitor/main.go
@@ -2,34 +2,43 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/Shopify/sarama"
 )
 
+var wg sync.WaitGroup
+
 func main() {
 	config := sarama.NewConfig()
-	config.Producer.RequiredAcks = sarama.WaitForAll          // 发送完数据需要leader和follow都确认
-	config.Producer.Partitioner = sarama.NewRandomPartitioner // 新选出一个partition
-	config.Producer.Return.Successes = true                   // 成功交付的消息将在success channel返回
+	//设置
+	//ack应答机制
+	config.Producer.RequiredAcks = sarama.WaitForAll
 
-	// 构造一个消息
+	//发送分区
+	config.Producer.Partitioner = sarama.NewRandomPartitioner
+
+	//回复确认
+	config.Producer.Return.Successes = true
+
+	//构造一个消息
 	msg := &sarama.ProducerMessage{}
-	msg.Topic = "web_log"
-	msg.Value = sarama.StringEncoder("this is a test log")
-	// 连接kafka
+	msg.Topic = "weatherStation"
+	msg.Value = sarama.StringEncoder("test:weatherStation device")
+
+	//连接kafka
 	client, err := sarama.NewSyncProducer([]string{"kafka0:9092", "kafka1:9093"}, config)
 	if err != nil {
-		fmt.Println("producer closed, err:", err)
-		return
+		fmt.Println("producer closed,err:", err)
 	}
 	defer client.Close()
-	// 发送消息
+
+	//发送消息
 	pid, offset, err := client.SendMessage(msg)
 	if err != nil {
-		fmt.Println("send msg failed, err:", err)
+		fmt.Println("send msg failed,err:", err)
 		return
 	}
-	fmt.Printf("pid:%v offset:%v\n", pid, offset)
+	fmt.Printf("pid:%v offset:%v \n ", pid, offset)
 
-	select {}
 }
