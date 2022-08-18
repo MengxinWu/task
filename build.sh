bc39d94e31627ddbd475a759d0e4ce5225c8e230
mengxin.wu <mengxin.wu@cootek.cn>
2022-08-18 17:41:09 +0800

    add and fix

---
+++ build.sh
@@ -0,0 +1,14 @@
+echo "start build docker images..."
+docker build -t admin:beta -f dockers/admin.Dockerfile .
+docker build -t dispatch:beta -f dockers/dispatch.Dockerfile .
+docker build -t monitor:beta -f dockers/monitor.Dockerfile .
+docker build -t monitor:beta -f dockers/monitor.Dockerfile .
+
+echo "remove docker container..."
+docker rm -f task_admin task_dispatch task_monitor task_executor
+
+echo "run docker container..."
+docker run -d --name task_admin --network tasknet --network-alias task_admin -p 48080:8080  admin:beta
+docker run -d --name task_dispatch --network tasknet --network-alias task_dispatch dispatch:beta
+docker run -d --name task_monitor --network tasknet --network-alias task_monitor monitor:beta
+docker run -d --name task_executor --network tasknet --network-alias task_executor executor:beta
