11a250f0e0b51af618c9b829e80e76451ebf69c0
mengxin.wu <mengxin.wu@cootek.cn>
2022-08-19 16:06:44 +0800

    add and fix

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ docker build -t monitor:beta -f dockers/monitor.Dockerfile .
 docker build -t executor:beta -f dockers/executor.Dockerfile .
 
 echo "remove docker container..."
-docker rm -f task_admin task_dispatch task_monitor task_executor
+docker rm -f task_admin
+docker rm -f task_dispatch
+docker rm -f task_monitor
+docker rm -f task_executor
 
 echo "run docker container..."
 docker run -d --name task_admin --network tasknet --network-alias task_admin -p 48080:8080  admin:beta
